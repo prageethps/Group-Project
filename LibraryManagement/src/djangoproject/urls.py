6906d82b5a70bf0b30b99f636f380a8e41b42ebe
prageethps <113235761+prageethps@users.noreply.github.com>
2022-10-12 16:26:36 +0530

    listing items & inputting data into database

--- LibraryManagement/src/djangoproject/urls.py
+++ LibraryManagement/src/djangoproject/urls.py
@@ -19,5 +19,7 @@ from librarymgmt import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('list_item/', views.list_item, name='list_item'),
+    path('add_items/', views.add_items, name='add_items'),
     path('admin/', admin.site.urls)
 ]
--- LibraryManagement/src/librarymgmt/views.py
+++ LibraryManagement/src/librarymgmt/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Books
+from .forms import BooksCreateForm
 # Create your views here.
 def home(request):
 	title = 'Welcome: This is the Home Page'
@@ -10,3 +11,22 @@ def home(request):
 
 	}
 	return render(request, "home.html",context)
+
+def list_item(request):
+	title = 'List of Items'
+	queryset = Books.objects.all()
+	context = {
+		"title": title,
+		"queryset": queryset,
+	}
+	return render(request, "list_item.html", context)
+
+def add_items(request):
+	form = BooksCreateForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+	context = {
+		"form": form,
+		"title": "Add Item",
+	}
+	return render(request, "add_items.html", context)
